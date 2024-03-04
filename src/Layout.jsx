a02ffb98734e7c7c47f033de722092944eacf262
Vignesh Mohan <vignesh.mohan@colanonline.com>
2024-03-04 19:35:32 +0530

    sidebar and navbar component completed

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -34,7 +34,7 @@ const Layout = () => {
           </Box>
           <Box
             component="main"
-            sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+            sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` },padding:"10px 40px" }}
           >
             <Toolbar />
             <Outlet/>
