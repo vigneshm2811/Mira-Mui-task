6994d2b0df314e92d3930f50ff4ae608137cf4ba
vigneshm2811 <vigneshmohan2811@gmail.com>
2025-04-29 10:56:08 +0530

    update Vite configuration to set base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Mira-Mui-task/', // 👈 this must match your repo name
 })
